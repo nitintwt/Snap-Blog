c5ea97d4943fc1e5b575cf501085bfd4753de3ff
Nitin Singh <nitinkumarsingh2368@gmail.com>
2024-01-14 15:58:38 +0530

    added redux toolkit

--- SnapBlog/src/App.jsx
+++ SnapBlog/src/App.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
-
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
 function App() {
-  console.log()
+  const [loading ,setLoading] =useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
 
 
-  return (
+
+  return !loading ? (
    <>
    <h1>Hello</h1>
    </>
-
-  )
+ 
+  ) : null
 }
 
 export default App
--- SnapBlog/src/appwrite/config.js
+++ SnapBlog/src/appwrite/config.js
@@ -27,7 +27,6 @@ export class Service{
          )
     } catch (error) {
       throw error 
-      
     }
 
   }
